3163116e1b7413af473ef4d286ddd43b7a3d690c
fathin-fadhil <fadhx7@gmail.com>
2024-10-26 23:58:35 +0700

    feat: adding loading and result view

--- src/app/dump/DumpForm.tsx
+++ src/app/dump/DumpForm.tsx
@@ -1,36 +1,52 @@
 "use client";
 
 import { useSearchParams } from "next/navigation";
+import { useFormState, useFormStatus } from "react-dom";
 
 export function DumpForm() {
   const searchParams = useSearchParams();
 
-  async function handleSubmit(formData: FormData) {
-    const data = formData.get("json_input");
-    try {
-      const res = await fetch("/api/dump", {
-        method: "POST",
-        body: data,
-        credentials: "include",
-        headers: {
-          "Content-Type": "application/json",
-          Authorization: `Bearer ${searchParams.get("admin")}`,
-        },
-      });
+  const [data, handleSubmit] = useFormState(
+    async (_: any, formData: FormData) => {
+      const data = formData.get("json_input");
+      try {
+        const res = await fetch("/api/dump", {
+          method: "POST",
+          body: data,
+          credentials: "include",
+          headers: {
+            "Content-Type": "application/json",
+            Authorization: `Bearer ${searchParams.get("admin")}`,
+          },
+        });
 
-      console.log(await res.json());
-    } catch (error) {
-      alert("submit error, see console for details");
-      console.error(error);
-    }
-  }
-  return (
-    <form action={handleSubmit}>
-      <label className="flex flex-col font-mono">
-        input json:
-        <textarea name="json_input" rows={10} className="border-2"></textarea>
-      </label>
-      <button type="submit">submit</button>
-    </form>
+        return await res.json();
+      } catch (error) {
+        return error;
+      }
+    },
+    {},
   );
+
+  return (
+    <>
+      <form action={handleSubmit}>
+        <label className="flex flex-col font-mono">
+          input json:
+          <textarea name="json_input" rows={10} className="border-2"></textarea>
+        </label>
+        <button type="submit">submit</button>
+        <LoadingIndicator />
+      </form>
+      <div className="">
+        <p className="pt-4 font-bold">result:</p>
+        <code>{JSON.stringify(data, null, 2)}</code>
+      </div>
+    </>
+  );
+}
+
+function LoadingIndicator() {
+  const { pending } = useFormStatus();
+  return pending ? <p>loading...</p> : <p>waiting...</p>;
 }
