2ef28aec6d43033dd10073cfa0cdb64aaef06264
fathin-fadhil <fadhx7@gmail.com>
2024-10-27 15:31:06 +0700

    feat: add server actions for retrieving nation data

---
+++ src/server/actions/nations.ts
@@ -0,0 +1,59 @@
+"use server";
+
+import { asyncCatchError } from "~/lib/utils";
+import { db } from "../db";
+import { InferSelectModel } from "drizzle-orm";
+
+export async function getNationData(nationCode: string) {
+  const [err, nation] = await asyncCatchError(
+    db.query.nations.findFirst({
+      where(nation, { eq }) {
+        return eq(nation.code, nationCode);
+      },
+      with: {
+        governmentForms: {
+          columns: {
+            governmentFormId: false,
+            id: false,
+            nationId: false,
+          },
+          with: {
+            governmentForm: true,
+          },
+        },
+        politicalSystems: {
+          columns: {
+            id: false,
+            nationId: false,
+            politicalSystemId: false,
+          },
+          with: {
+            politicalSystem: true,
+          },
+        },
+        headOfGovernments: true,
+        headOfStates: true,
+        parties: true,
+      },
+    }),
+  );
+
+  if (!nation) return { found: false as const, nation: null };
+
+  return { found: true as const, nation: nation };
+}
+
+export async function getAllNations() {
+  const [err, nations] = await asyncCatchError(
+    db.query.nations.findMany({
+      columns: {
+        id: true,
+        code: true,
+        name: true,
+        slug: true,
+      },
+    }),
+  );
+
+  return nations as { id: string; code: string; name: string; slug: string }[];
+}
