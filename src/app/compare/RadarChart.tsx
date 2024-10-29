29393687c13ab9352716e2d212e3e5fa680fa8b2
fathin-fadhil <fadhx7@gmail.com>
2024-10-29 23:25:18 +0700

    fix: radar sizing based on screen width

--- src/app/compare/RadarChart.tsx
+++ src/app/compare/RadarChart.tsx
@@ -2,10 +2,20 @@
 import { type Nation } from "~/server/actions/nations";
 import { useECharts } from "@kbox-labs/react-echarts";
 import { type UseEChartsOptions } from "@kbox-labs/react-echarts";
-import { useEffect, useState } from "react";
+import {
+  startTransition,
+  useEffect,
+  useRef,
+  useState,
+  useTransition,
+} from "react";
 import { stringToRGBA } from "~/lib/utils";
 
-function getChartOption(nations: Nation[]): UseEChartsOptions {
+function getChartOption(
+  nations: Nation[],
+  radarCenter: [string, string] = ["50%", "50%"],
+  radarRadius = "70%",
+): UseEChartsOptions {
   const option: UseEChartsOptions = {
     textStyle: {
       fontFamily: "sans-serif, mono",
@@ -24,8 +34,9 @@ function getChartOption(nations: Nation[]): UseEChartsOptions {
       })),
     },
     radar: {
-      radius: "70%",
-      scale: false,
+      radius: radarRadius,
+      scale: true,
+      center: radarCenter,
       indicator: [
         { name: "Pembangunan\nManusia", max: 1, axisType: "value" },
         { name: "Persepsi\nKorupsi", max: 100, axisType: "value" },
@@ -62,21 +73,22 @@ function getChartOption(nations: Nation[]): UseEChartsOptions {
       ],
     })),
   };
-  /*   console.log("🚀 ~ getChartOption ~ option:", option); */
 
   return option;
 }
 
 export default function RadarChart({ nations }: { nations: Nation[] }) {
-  const [ref, ecInstance] = useECharts<HTMLDivElement>(getChartOption(nations));
-  const [screenWidth, setScreenWidth] = useState({
-    width: window.innerWidth,
-    height: window.innerHeight,
-  });
+  const radarRadius = useRef<string>("70%");
+  const [ref, ecInstance] = useECharts<HTMLDivElement>(
+    getChartOption(nations, undefined, radarRadius.current),
+  );
+  const [screenWidth, setScreenWidth] = useState(window.innerWidth);
 
   useEffect(() => {
     function onResize() {
-      setScreenWidth({ width: window.innerWidth, height: window.innerHeight });
+      startTransition(() => {
+        setScreenWidth(window.innerWidth);
+      });
     }
 
     window.addEventListener("resize", onResize);
@@ -89,8 +101,19 @@ export default function RadarChart({ nations }: { nations: Nation[] }) {
   useEffect(() => {
     ecInstance?.clear();
     ecInstance?.setOption(getChartOption(nations));
-    console.log("🚀 ~ useEffect ~ nations:", nations);
   }, [nations]);
 
-  return <div className="mx-auto aspect-[10/5] w-full" ref={ref}></div>;
+  useEffect(() => {
+    if (screenWidth < 545) radarRadius.current = "55%";
+    else if (screenWidth < 705) radarRadius.current = "60%";
+    else radarRadius.current = "70%";
+    ecInstance?.resize();
+  }, [screenWidth]);
+
+  return (
+    <div
+      className="mx-auto aspect-[10/10] w-full sm:aspect-[10/6] md:aspect-[10/5]"
+      ref={ref}
+    ></div>
+  );
 }
