5e33eb3d06a635e8322bcc64aee9cd3eb9b7934c
fathin-fadhil <fadhx7@gmail.com>
2024-10-31 01:46:48 +0700

    feat: updated schema for storing longer gov form desc in markdown

---
+++ drizzle/0002_normal_mystique.sql
@@ -0,0 +1,3 @@
+ALTER TABLE "government_form" ADD COLUMN "long_description_md" text;--> statement-breakpoint
+ALTER TABLE "head_of_government" ADD COLUMN "long_description_md" text;--> statement-breakpoint
+ALTER TABLE "head_of_state" ADD COLUMN "long_description_md" text;
