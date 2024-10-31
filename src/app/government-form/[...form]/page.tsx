ce88d6e57a8b6a311292bc3d5645d7d02e6ecacf
fathin-fadhil <fadhx7@gmail.com>
2024-10-31 22:19:38 +0700

    feat: list all nation with the same government form

--- src/app/government-form/[...form]/page.tsx
+++ src/app/government-form/[...form]/page.tsx
@@ -4,6 +4,9 @@ import { getGovernmentFormData } from "~/server/actions/nations";
 import Markdown from "react-markdown";
 import { Card, CardContent, CardHeader } from "~/components/ui/card";
 import { BackButton } from "~/components/back-button";
+import { buttonVariants } from "~/components/ui/button";
+import Link from "next/link";
+import { ExternalLink } from "lucide-react";
 
 export default async function GovernmentFormPage({
   params,
@@ -33,6 +36,34 @@ export default async function GovernmentFormPage({
                     <p>{governmentFormData.description}</p>
                   </>
                 )}
+                <hr />
+                <h2>
+                  Negara-Negara Dengan Bentuk Pemerintahan{" "}
+                  {governmentFormData.name}
+                </h2>
+                <div className="not-prose grid grid-cols-1 md:grid-cols-2">
+                  {governmentFormData.govermentFormsToNations.map(
+                    ({ nation }) => (
+                      <div key={nation.id} className="p-2">
+                        <Link
+                          className={buttonVariants({
+                            variant: "secondary",
+                            className: "w-full",
+                          })}
+                          href={`/compare?nations=${nation.code}`}
+                        >
+                          <img
+                            src={` https://flagcdn.com/w20/${nation.code}.png `}
+                            width={20}
+                            className="h-fit"
+                          ></img>
+                          {nation.name}
+                          <ExternalLink className="h-5 w-5" />
+                        </Link>
+                      </div>
+                    ),
+                  )}
+                </div>
               </article>
             </CardContent>
           </Card>
