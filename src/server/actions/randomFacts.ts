461d83b76380a6191e5a8ceef4de10a80ae2f833
fathin-fadhil <fadhx7@gmail.com>
2024-11-07 23:19:30 +0700

    fix: prevent same fact to be shown twice

--- src/server/actions/randomFacts.ts
+++ src/server/actions/randomFacts.ts
@@ -11,6 +11,10 @@ export async function getRandomFact(count: number): Promise<RandomFact[]> {
   const facts: RandomFact[] = [];
   for (let i = 0; i < count; i++) {
     const index = getRandomIndex(randomFact.length);
+    if (facts.find((fact) => fact.code === randomFact[index]!.code)) {
+      i--;
+      continue;
+    }
     facts.push(randomFact[index]!);
   }
   return facts;
